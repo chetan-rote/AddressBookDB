73b9a88f0182cbc34a28289948fac0b1c9ac54ec
chetan-rote <55949445+chetan-rote@users.noreply.github.com>
2020-11-20 12:31:06 +0530

    [Chetan]Add. Create address book table.

--- AddressBookCRUDOperations.sql
+++ AddressBookCRUDOperations.sql
@@ -3,3 +3,15 @@
 create database addressBook_Services
 /*Switching to Address book Database*/
 use addressBook_Services
+/*UC2*/
+/*Creating Address book table*/
+create table addressBook_Table
+( firstName varchar(50) not null,
+ secondName varchar(50) not null,
+ address varchar(50) not null,
+ city varchar(50) not null,
+ state varchar(50) not null,
+ zip bigint not null,
+ phoneNumber bigint not null,
+ emailId varchar(50) not null
+ );
